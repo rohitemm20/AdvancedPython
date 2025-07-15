60eef0a9b5833b824e75570f4193015a4f0fe20d
rohitemm20 <rohit.emmanuel2023@vitstudent.ac.in>
2025-07-15 06:56:16 +0530

    Create currencynote.py

---
+++ currencynote.py
@@ -0,0 +1,24 @@
+
+language= [
+    ["Assamese", 52],
+    ["Bengali", 50],
+    ["Gujarati", 48],
+    ["Kannada", 49],
+    ["Kashmiri", 117], 
+    ["Konkani", 46],    
+    ["Malayalam", 53],
+    ["Marathi", 50],
+    ["Nepali", 50],
+    ["Odia", 50],
+    ["Punjabi", 41],
+    ["Sanskrit", 50],
+    ["Tamil", 247],  
+    ["Telugu", 60],
+    ["Urdu", 39]       
+]
+
+
+print("Language  Characters")
+print("\t")
+for row in language:
+    print(f"{row[0]:<10}  {row[1]}")
